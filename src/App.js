6c6a404dbe142f4eafd61d922e23f5d4ce6abc34
manaswinisreekar <you@example.com>
2020-09-11 21:51:50 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React, { Component } from 'react';
-import { Fragment } from 'react';
+import Header from './Header';
+import './App.css';
 
 
 class App extends Component {
   render() {
+    let subscribers=[
+      {
+        name:"Shaunak",
+        phone:12345678
+      },
+      {
+        name:"apple",
+        phone:214567895
+      }
+    ]
     return (
   
-    <div>
+    <div className="component-container">
       <Header/>
-        <button> Add</button>
-        <div>
-          <span>Name</span><br/>
-          <span> Phone</span>
+      <div className="component-body-container">
+
+        <button className="custom-btn add-btn"> Add</button>
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading">shaunak</span><br/>
+          <span className ="grid-item phone-heading">12345678</span>
         </div>
 
-    </div>
+        {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+              </div>
+            })
+          }
+        </div>
+      </div>
     );
-    }
-  }
-  export default App;
-
-  
-
-
-        
-        
-        
-        
-        
-
-
-
-
+        }
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Header from './Header';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import Header from './Header';
 
+//Functional Component
 const Header= function(){
    return(
        <div className="header">
@@ -9,9 +10,15 @@ const Header= function(){
        </div>
    )
 
-        
-           
-        
-    }
+
+   //const Header= function(){
+    //   const headerStyle={textAlign:'center', padding:20, background:#0000,color:#fff, textTransform:'uppercase'}
+   // return(
+       // <div style={headerStyle}>
+       //     Phone Directory
+       // </div>
+  //  )  
+   // }
+   }
  
 export default Header;
