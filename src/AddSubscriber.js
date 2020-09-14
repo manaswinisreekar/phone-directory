37ff14ad130465db9c13b24534c249190582620c
manaswinisreekar <you@example.com>
2020-09-14 18:02:36 +0530

    debugged App.js

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -5,29 +5,31 @@ import './AddSubscriber.css';
 class AddSubscriber extends Component{
     render(){
         return{
-            <div>
-            <Header heading="Add Subscriber" />
-            <div className="component-body-container">
-            <button className="custom-btn">Back</button>
-            <form className="subscriber-form">
-            <label htmlFor="name" className="label-control">Name</label></br>
-            <input id="name" type="text" className="input-control" name="name"></input> <br/><br/>
-            <label htmlFor="phone" className="label-control">Phone</label>   <br/>
-            <input id="phone" type="text" className="input-control" name="name"></input> <br/><br/>
-            <div className="subscriber-info-container">
-            </div>
-             <span className="Add to heading">Subscriber to be added</span><br/>
-             <span className="subscriber-info">Name</span><br/>
-             <span classNmae="subscriber-info">phone</span><br/>
-              
-
-            </form>
-            <button type="submit" className="custom -btn add-btn">Add</button>
-            </div>
-            </div>
-
-
-        }
-    }
-}
+            class AddSubscriber extends Component {
+                render() {
+                    return (
+                        <div>
+                            <Header heading="Add Subscriber" />
+                            <div className="component-body-container">
+                                <button className="custom-btn">Back</button>
+            
+                                <form className="subscriber-form">
+                                    <label htmlFor="name" className="label-control">Name: </label><br/>
+                                    <input id="name" type="text" className="input-control" name="name"/><br/><br/>
+                                    <label htmlFor="phone" className="label-control">Phone: </label><br/>
+                                    <input id="phone" type="text" className="input-control" name="phone"/><br/><br/>
+            
+                                    <div className="subscriber-info-container">
+                                        <span className="subscriber-to-add-heading">Subscriber to be added: </span><br/>
+                                        <span className="subscriber-info">Name: </span><br/>
+                                        <span className="subscriber-info">Phone: </span><br/>
+                                    </div>
+            
+                                    <button type="submit" className="custom-btn add-btn">Add</button>
+                                </form>
+                            </div>
+                        </div>
+                    )
+                }
+            }
 export default AddSubscriber;
